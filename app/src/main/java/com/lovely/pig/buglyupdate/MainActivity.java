355f76b315519651adbbdca863bc41eeed7a4cf2
swg <shiweigang789@126.com>
2019-04-10 16:58:37 +0800

    添加bugly更新

---
+++ app/src/main/java/com/lovely/pig/buglyupdate/MainActivity.java
@@ -0,0 +1,24 @@
+package com.lovely.pig.buglyupdate;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv=(TextView)findViewById(R.id.tv);
+      //  String str=null;
+//        String str="老表真帅";
+        String str="老表真帅3333333333";
+         str="老表真帅3333333333";
+         str="老表真帅3333333333";
+         str="老表真帅3333333333";
+         str="老表真帅3333333333";
+         str="老表真帅3333333333";
+        tv.setText(str);
+    }
+}
--- app/src/main/java/com/lovely/pig/buglyupdate/SampleApplicationLike.java
+++ app/src/main/java/com/lovely/pig/buglyupdate/SampleApplicationLike.java
@@ -13,6 +13,8 @@ import com.tencent.bugly.beta.Beta;
 import com.tencent.bugly.beta.interfaces.BetaPatchListener;
 import com.tencent.tinker.loader.app.DefaultApplicationLike;
 
+import java.util.Locale;
+
 
 /**
  * 自定义ApplicationLike类.
@@ -24,108 +26,106 @@ import com.tencent.tinker.loader.app.DefaultApplicationLike;
  */
 public class SampleApplicationLike extends DefaultApplicationLike {
 
-    public static final String TAG = "Tinker.SampleApplicationLike";
+	public static final String TAG = "Tinker.SampleApplicationLike";
 
-    public SampleApplicationLike(Application application, int tinkerFlags,
+	public SampleApplicationLike(Application application, int tinkerFlags,
 								 boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime,
 								 long applicationStartMillisTime, Intent tinkerResultIntent) {
-        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime,
-                applicationStartMillisTime, tinkerResultIntent);
-    }
+		super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime,
+				applicationStartMillisTime, tinkerResultIntent);
+	}
 
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        // 设置是否开启热更新能力，默认为true
-        Beta.enableHotfix = true;
-        // 设置是否自动下载补丁，默认为true
-        Beta.canAutoDownloadPatch = true;
-        // 设置是否自动合成补丁，默认为true
-        Beta.canAutoPatch = true;
-        // 设置是否提示用户重启，默认为false
-        Beta.canNotifyUserRestart = false;
-        // 补丁回调接口
-        Beta.betaPatchListener = new BetaPatchListener() {
-            @Override
-            public void onPatchReceived(String patchFile) {
-              //  Toast.makeText(getApplication(), "补丁下载地址" + patchFile, Toast.LENGTH_SHORT).show();
-            }
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		// 设置是否开启热更新能力，默认为true
+		Beta.enableHotfix = true;
+		// 设置是否自动下载补丁，默认为true
+		Beta.canAutoDownloadPatch = true;
+		// 设置是否自动合成补丁，默认为true
+		Beta.canAutoPatch = true;
+		// 设置是否提示用户重启，默认为false
+		Beta.canNotifyUserRestart = false;
+		// 补丁回调接口
+		Beta.betaPatchListener = new BetaPatchListener() {
+			@Override
+			public void onPatchReceived(String patchFile) {
+				Toast.makeText(getApplication(), "补丁下载地址" + patchFile, Toast.LENGTH_SHORT).show();
+			}
 
-            @Override
-            public void onDownloadReceived(long savedLength, long totalLength) {
-              /*  Toast.makeText(getApplication(),
-                        String.format(Locale.getDefault(), "%s %d%%",
-                                Beta.strNotificationDownloading,
-                                (int) (totalLength == 0 ? 0 : savedLength * 100 / totalLength)),
-                        Toast.LENGTH_SHORT).show();*/
-            }
+			@Override
+			public void onDownloadReceived(long savedLength, long totalLength) {
+				Toast.makeText(getApplication(),
+						String.format(Locale.getDefault(), "%s %d%%",
+								Beta.strNotificationDownloading,
+								(int) (totalLength == 0 ? 0 : savedLength * 100 / totalLength)),
+						Toast.LENGTH_SHORT).show();
+			}
 
-            @Override
-            public void onDownloadSuccess(String msg) {
-                Toast.makeText(getApplication(), "应用已更新成功", Toast.LENGTH_SHORT).show();
+			@Override
+			public void onDownloadSuccess(String msg) {
+				Toast.makeText(getApplication(), "应用已更新成功", Toast.LENGTH_SHORT).show();
 
-            }
+			}
 
-            @Override
-            public void onDownloadFailure(String msg) {
-                Toast.makeText(getApplication(), "补丁下载失败", Toast.LENGTH_SHORT).show();
+			@Override
+			public void onDownloadFailure(String msg) {
+				Toast.makeText(getApplication(), "补丁下载失败", Toast.LENGTH_SHORT).show();
 
-            }
+			}
 
-            @Override
-            public void onApplySuccess(String msg) {
-              //  Toast.makeText(getApplication(), "应用已更新成功", Toast.LENGTH_SHORT).show();
+			@Override
+			public void onApplySuccess(String msg) {
+				  Toast.makeText(getApplication(), "应用已更新成功", Toast.LENGTH_SHORT).show();
+			}
 
+			@Override
+			public void onApplyFailure(String msg) {
+				Toast.makeText(getApplication(), "补丁应用失败", Toast.LENGTH_SHORT).show();
 
-            }
+			}
 
-            @Override
-            public void onApplyFailure(String msg) {
-                Toast.makeText(getApplication(), "补丁应用失败", Toast.LENGTH_SHORT).show();
+			@Override
+			public void onPatchRollback() {
 
-            }
-
-            @Override
-            public void onPatchRollback() {
-
-            }
-        };
+			}
+		};
 
 
-         //设置通知栏大图标，largeIconId为项目中的图片资源;
+		//设置通知栏大图标，largeIconId为项目中的图片资源;
 
-         Beta.largeIconId = R.mipmap.ic_launcher;
-        // 设置开发设备，默认为false，上传补丁如果下发范围指定为“开发设备”，需要调用此接口来标识开发设备
-        Bugly.setIsDevelopmentDevice(getApplication(),false);
-        // 多渠道需求塞入
+		Beta.largeIconId = R.mipmap.ic_launcher;
+		// 设置开发设备，默认为false，上传补丁如果下发范围指定为“开发设备”，需要调用此接口来标识开发设备
+		Bugly.setIsDevelopmentDevice(getApplication(), false);
+		// 多渠道需求塞入
 //        String channel = WalleChannelReader.getChannel(getApplication());
-        Bugly.setAppChannel(getApplication(), "default");
-        // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId
-        Bugly.init(getApplication(), "35ede8d0a1", true);
-    }
+		Bugly.setAppChannel(getApplication(), "default");
+		// 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId
+		Bugly.init(getApplication(), "35ede8d0a1", true);
+	}
 
 
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    @Override
-    public void onBaseContextAttached(Context base) {
-        super.onBaseContextAttached(base);
-        // you must install multiDex whatever tinker is installed!
-        MultiDex.install(base);
+	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+	@Override
+	public void onBaseContextAttached(Context base) {
+		super.onBaseContextAttached(base);
+		// you must install multiDex whatever tinker is installed!
+		MultiDex.install(base);
 
-        // TODO: 安装tinker
-        Beta.installTinker(this);
-    }
+		// TODO: 安装tinker
+		Beta.installTinker(this);
+	}
 
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    public void registerActivityLifecycleCallback(
-            Application.ActivityLifecycleCallbacks callbacks) {
-        getApplication().registerActivityLifecycleCallbacks(callbacks);
-    }
+	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+	public void registerActivityLifecycleCallback(
+			Application.ActivityLifecycleCallbacks callbacks) {
+		getApplication().registerActivityLifecycleCallbacks(callbacks);
+	}
 
-    @Override
-    public void onTerminate() {
-        super.onTerminate();
-        Beta.unInit();
-    }
+	@Override
+	public void onTerminate() {
+		super.onTerminate();
+		Beta.unInit();
+	}
 }
